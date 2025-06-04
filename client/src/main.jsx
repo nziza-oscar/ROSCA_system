5585a9f48fd6ec6df820f5d07edcfb14add4d41a
Nziza Oscar <santosjuniorsoftwareengineer1@gmail.com>
2025-06-04 17:32:40 +0200

    notifcations

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -12,3 +12,10 @@ createRoot(document.getElementById('root')).render(
     </Provider>
   </StrictMode>,
 )
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(reg => console.log('service Worker registered:', reg))
+      .catch(err => console.error('Service Worker registration failed:', err));
+  });
+}
